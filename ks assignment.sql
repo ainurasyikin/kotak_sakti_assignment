c0c0c68adf9314ab2f78d633a17c60fd81fd4f4c
Ainur Asyikin Zulkornain <116057562+ainurasyikin@users.noreply.github.com>
2023-11-19 12:56:39 +0800

    Update ks assignment.sql

--- ks assignment.sql
+++ ks assignment.sql
@@ -31,9 +31,6 @@ VALUES
 (6,	'Shanon Teoh' , 'shahnon.teoh@st.com.sg' , '', '2019-08-07 08:13:21' , '2019-08-07 08:13:21' ) 
 
 
-<img width="530" alt="image" src="https://github.com/ainurasyikin/ks_assignment/assets/116057562/4742f5e2-a31a-4471-aceb-9c0a5d1886be">
-
-
 ### Create table invoices ####
 
 
